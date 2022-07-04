750727b0d092ba10729ad0e900b863a4ed88495b
Rohith <marganirohith2000@gmail.com>
2022-07-04 15:16:35 +0530

    schema

--- models/isssue.js
+++ models/isssue.js
@@ -47,5 +47,5 @@ const issueSchema = new Schema( //defines the structure
   { timestamps: true }
 );
 
-const Issue = mongoose.model("Issue", issueSchema); //blog model
+const Issue = mongoose.model("Issue", issueSchema); //issue model
 module.exports = Issue;
